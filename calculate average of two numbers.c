8f628076b0e4237344ac97701ad7e20f9fdd997e
Adi1ya <adityamaji474@gmail.com>
2022-02-18 21:27:04 +0530

    Add files via upload

---
+++ calculate average of two numbers.c
@@ -0,0 +1,13 @@
+// calculate the average of two number with variables
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int a=5,b=10;
+float avg;
+avg=(a+b)/2;
+printf("The average of A B is :%f ",avg);
+getch();
+}
+//this code is written by Ad1tya 
